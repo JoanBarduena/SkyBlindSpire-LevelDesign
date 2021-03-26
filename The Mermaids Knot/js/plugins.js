30446c3f81e282ef7666496d17cc17a664ad0866
Xavi Marín Solà <xmarin98@gmail.com>
2021-03-26 14:12:52 +0100

    added some new plugins :100:

--- The Mermaids Knot/js/plugins.js
+++ The Mermaids Knot/js/plugins.js
@@ -3,5 +3,7 @@
 var $plugins =
 [
 {"name":"Community_Basic","status":true,"description":"Basic plugin for manipulating important parameters.","parameters":{"cacheLimit":"20","screenWidth":"816","screenHeight":"624","changeWindowWidthTo":"","changeWindowHeightTo":"","renderingMode":"auto","alwaysDash":"off"}},
-{"name":"MadeWithMv","status":true,"description":"Show a Splash Screen \"Made with MV\" and/or a Custom Splash Screen before going to main screen.","parameters":{"Show Made With MV":"true","Made with MV Image":"MadeWithMv","Show Custom Splash":"false","Custom Image":"","Fade Out Time":"120","Fade In Time":"120","Wait Time":"160"}}
+{"name":"MadeWithMv","status":false,"description":"Show a Splash Screen \"Made with MV\" and/or a Custom Splash Screen before going to main screen.","parameters":{"Show Made With MV":"true","Made with MV Image":"MadeWithMv","Show Custom Splash":"false","Custom Image":"","Fade Out Time":"120","Fade In Time":"120","Wait Time":"160"}},
+{"name":"MOG_TreasureHud","status":true,"description":"(v1.3) Apresenta uma Hud com os tesouros adquiridos no jogo.","parameters":{"Hud X-Axis":"555","Hud Y-Axis":"500","Name X-Axis":"78","Name Y-Axis":"24","Icon X-Axis":"42","Icon Y-Axis":"24","Duration":"90","Gold Icon Index":"163","Font Size":"20"}},
+{"name":"MOG_MapNameHud","status":true,"description":"(v1.3) Apresenta uma Hud com o nome do mapa.","parameters":{"Hud X-Axis":"250","Hud Y-Axis":"60","Name X-Axis":"80","Name Y-Axis":"15","Duration":"160","Font Size":"20","Font Italic":"false","Slide Animation":"true","Zoom Animation":"false","Zoom Text Animation":"true","Particle Animation":"true","Particles Number":"15","Circle Animation":"true","Circle X-Axis":"-120","Circle Y-Axis":"0","Circle Z-Index":"0","Circle Rotation Speed":"0.01"}}
 ];
